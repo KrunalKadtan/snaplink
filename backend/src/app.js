7ccdfa52932e8c94a248dc6ef18bfd21e24afe87
Krunal <krunalkadtan19@gmail.com>
2026-03-29 10:30:12 +0000

    feat: build routes for users & urls

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import userRoutes from './routes/user.routes.js'
+import urlRoutes from './routes/url.routes.js'
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/api/users', userRoutes)
+app.use('/api/urls', urlRoutes)
 
 export default app
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import userController from './../controllers/user.controller.js'
+
+const router = Router()
+
+router.post('/register', userController.registerUser)
+router.post('/login', userController.loginUser)
+router.post('/logout', userController.logoutUser)
+
+export default router
